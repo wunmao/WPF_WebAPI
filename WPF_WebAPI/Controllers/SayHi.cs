51855fe3137ef4fcf5727d09aa94f9488edba963
wunmao <wunmao@live.com>
2022-12-23 22:36:25 +0800

    加入專案檔案。

---
+++ WPF_WebAPI/Controllers/SayHi.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WPF_WebAPI.Controllers;
+
+[Route(nameof(SayHi))]
+public class SayHi : ControllerBase
+{
+    [HttpGet]
+    [Route(nameof(Hello))]
+    public IActionResult Hello(string name) => Ok($"Hello {name}");
+
+    [HttpGet]
+    [Route(nameof(Hi))]
+    public IActionResult Hi(string name) => Ok($"Hi, {name}");
+}
---
+++ WPF_WebAPI/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WPF_WebAPI;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow() => InitializeComponent();
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        var builder = WebApplication.CreateBuilder();
+
+        // Add services to the container.
+
+        _=builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        _=builder.Services.AddEndpointsApiExplorer();
+        _=builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            _=app.UseSwagger();
+            _=app.UseSwaggerUI();
+        }
+
+        _=app.UseHttpsRedirection();
+
+        _=app.UseAuthorization();
+
+        _=app.MapControllers();
+
+        app.Run();
+    }
+}
