401f42c4326b874bfd0162d026ed2c1309b794ad
wunmao <wunmao@live.com>
2022-12-23 22:38:05 +0800

    20221223

--- WPF_WebAPI/MainWindow.xaml.cs
+++ WPF_WebAPI/MainWindow.xaml.cs
@@ -1,13 +1,11 @@
 ﻿using System.Windows;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace WPF_WebAPI;
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
+
+/// <summary>Interaction logic for MainWindow.xaml</summary>
 public partial class MainWindow : Window
 {
     public MainWindow() => InitializeComponent();
@@ -15,28 +13,22 @@ public partial class MainWindow : Window
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
         var builder = WebApplication.CreateBuilder();
-
-        // Add services to the container.
-
-        _=builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        _=builder.Services.AddEndpointsApiExplorer();
-        _=builder.Services.AddSwaggerGen();
+        _ = builder.Services.AddControllers();
+        _ = builder.Services.AddEndpointsApiExplorer();
+        _ = builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            _=app.UseSwagger();
-            _=app.UseSwaggerUI();
+            _ = app.UseSwagger();
+            _ = app.UseSwaggerUI();
         }
 
-        _=app.UseHttpsRedirection();
+        _ = app.UseHttpsRedirection();
 
-        _=app.UseAuthorization();
+        _ = app.UseAuthorization();
 
-        _=app.MapControllers();
+        _ = app.MapControllers();
 
         app.Run();
     }
