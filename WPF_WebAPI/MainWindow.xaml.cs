c7373ac7b12fcb0f00c6ad0ec6d92cdd923f6270
wunmao <wunmao@live.com>
2022-12-23 23:30:20 +0800

    20221223

--- WPF_WebAPI/MainWindow.xaml.cs
+++ WPF_WebAPI/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.OpenApi;
 
 namespace WPF_WebAPI;
 
